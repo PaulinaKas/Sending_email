e0b5c7b57480e52c89ee2d34d0cb01c87eab0d53
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-01-13 17:01:38 +0100

    Adjustments for comments

--- GIT_emailFV.py
+++ GIT_emailFV.py
@@ -76,40 +76,44 @@ class EditMenu:
 		textPad.event_generate("<<Copy>>")
 	def paste(self):
 		textPad.event_generate("<<Paste>>")
-	def search_for(needle,cssnstv, textPad, t2,e) :
-			textPad.tag_remove('match', '1.0', END)
-			count =0
-			if needle:
-					pos = '1.0'
-					while True:
-						pos = textPad.search(needle, pos, nocase=cssnstv, stopindex=END)
-						if not pos: break
-						lastpos = '%s+%dc' % (pos, len(needle))
-						textPad.tag_add('match', pos, lastpos)
-						count += 1
-						pos = lastpos
-					textPad.tag_config('match', foreground='white', background='deepskyblue3')
+	def search_for(needle,cssnstv, textPad, t2,e):
+		textPad.tag_remove('match', '1.0', END)
+		count =0
+		if needle:
+			pos = '1.0'
+			while True:
+				pos = textPad.search(needle, pos, nocase=cssnstv, stopindex=END)
+				if not pos: break
+				lastpos = '%s+%dc' % (pos, len(needle))
+				textPad.tag_add('match', pos, lastpos)
+				count += 1
+				pos = lastpos
+				textPad.tag_config('match', foreground='white',
+								   background='deepskyblue3')
 			e.focus_set()
 			t2.title('%d matches found' %count)
 	def on_find(self):
-			t2 = Toplevel(root)
-			t2.title('Find')
-			t2.geometry('400x100+200+250')
-			t2.transient(root)
-			Label(t2, text="Find All:").grid(row=0, column=0, sticky='e')
-			v=StringVar()
-			e = Entry(t2, width=25, textvariable=v)
-			e.grid(row=0, column=1, padx=2, pady=2, sticky='we')
-			e.focus_set()
-			c=IntVar()
-			Checkbutton(t2, text='Ignore Case', variable=c).grid(row=1, column=1, sticky='e', padx=2, pady=2)
-			Button(t2, text="Find All", underline=0,  command=lambda: EditMenu.search_for(v.get(),c.get(), textPad, t2,e)).grid(row=0, column=2, sticky='e'+'w', padx=2, pady=2)
+		t2 = Toplevel(root)
+		t2.title('Find')
+		t2.geometry('400x100+200+250')
+		t2.transient(root)
+		Label(t2, text="Find All:").grid(row=0, column=0, sticky='e')
+		v=StringVar()
+		e = Entry(t2, width=25, textvariable=v)
+		e.grid(row=0, column=1, padx=2, pady=2, sticky='we')
+		e.focus_set()
+		c=IntVar()
+		Checkbutton(t2, text='Ignore Case', variable=c).grid(row=1,
+							  column=1, sticky='e', padx=2, pady=2)
+		Button(t2, text="Find All", underline=0,
+			   command=lambda: EditMenu.search_for(v.get(),c.get(), textPad, t2,e)).grid(
+			   row=0, column=2, sticky='e'+'w', padx=2, pady=2)
 
-			def close_search():
-					textPad.tag_remove('match', '1.0', END)
-					t2.destroy()
+		def close_search():
+				textPad.tag_remove('match', '1.0', END)
+				t2.destroy()
 
-			t2.protocol('WM_DELETE_WINDOW', close_search)
+		t2.protocol('WM_DELETE_WINDOW', close_search)
 
 editMenuObject = EditMenu()
 
@@ -122,7 +126,8 @@ class FileMenu:
 		root.title("Untitled")
 		textPad.delete(1.0,END)
 	def exit_editor(event=None):
-		if messagebox.askokcancel("Quit?", "Do you really want to exit?", icon = 'warning'):
+		if messagebox.askokcancel("Quit?", "Do you really want to exit?",
+								  icon = 'warning'):
 			root.destroy()
 	def save_as_function(self):
 		fileToSave = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
@@ -142,7 +147,8 @@ class BottomToolbar:
 		openedFile = open(fileToOpen, 'w')
 		fromFirstLineContent = textPad.get(1.0, END)
 		openedFile.write(fromFirstLineContent)
-		MsgBox = messagebox.askquestion('Warning','Are you sure you want to overwrite the file?', icon = 'warning')
+		MsgBox = messagebox.askquestion('Warning',
+			  'Are you sure you want to overwrite the file?', icon = 'warning')
 		if MsgBox == 'Yes':
 			try:
 				f.close()
@@ -156,15 +162,18 @@ class BottomToolbar:
 		Info:
 		Something like '---------------------------------' separates content
 		to send from content that has been send last time.
-		Below variable "separatingIndex" is set of rows that equal to '---------------------------------'.
+		Below variable "separatingIndex" is set of rows
+		that equal to '---------------------------------'.
 		Thanks to it program can distinguish last new content that must be send.
 		'''
 		root.clipboard_clear()
 		with open(fileToOpen, encoding='utf-8') as file:
 			fileContent = file.readlines()
 			separatingIndex = [x for x in range(len(fileContent)) if '---------------------------------' in fileContent[x]]
-		fileToExport = filedialog.asksaveasfile(initialfile='export.csv', mode='w', defaultextension='.csv')
-		if fileToExport is None: # asksaveasfile returns `None` if dialog closed with "cancel".
+		fileToExport = filedialog.asksaveasfile(initialfile='export.csv',
+									            mode='w', defaultextension='.csv')
+		# below: asksaveasfile returns `None` if dialog closed with "cancel".
+		if fileToExport is None:
 			return
 		newContent = textPad.get(float(separatingIndex[-1]+2), END)
 		fileToExport.write(newContent)
@@ -172,7 +181,8 @@ class BottomToolbar:
 
 	def sendBig(self):
 		'''
-		Below is a big function which sends e-mail with proper title, body and attachments.
+		Below is a big function which sends e-mail with proper title,
+		body and attachments.
 		'''
 		class Subject:
 			weekDictionary = {	1: 'wtorek',
@@ -198,22 +208,25 @@ class BottomToolbar:
 				self.attachmentsDirectory = attachmentsDirectory
 
 			def createBody(self):
-				mailBody = '<font face="verdana, monospace">' + "Cześć tatuś, oto fakturki :)" + \
-						  '</font>' +  "<br>"
+				mailBody = '<font face="verdana, monospace">' + \
+						   "Cześć tatuś, oto fakturki :)" + \
+						   '</font>' +  "<br>"
 				with open(self.contentToSend, encoding='utf-8') as file:
 					content = csv.reader(file, delimiter =';')
 
 					for row in content:
 						if row[0] != 'sent':
-							mailBody += '<font face="verdana, monospace">' + "<br>" + \
-							"   - " + '<b>' + row[1] + ' '+ '</b>' + row[2] + ' ' + \
-							row[3] + ' ' + row[4] + '<b>' + '<font color="MidnightBlue">' + \
-							row[5] + ' ' + '</font>' + '<font color="MediumVioletRed">' + \
-							row [6] + ' ' +  '<font color="OrangeRed">' + row[7] + ' ' + \
-							'</font>' + '</font>' + '</b> <br>'
+							mailBody += '<font face="verdana, monospace">' + \
+							"<br>" + "   - " + '<b>' + row[1] + ' '+ '</b>' + \
+							row[2] + ' ' + row[3] + ' ' + row[4] + '<b>' + \
+							'<font color="MidnightBlue">' + row[5] + ' ' + \
+							'</font>' + '<font color="MediumVioletRed">' + \
+							row [6] + ' ' +  '<font color="OrangeRed">' + \
+							row[7] + ' ' + '</font>' + '</font>' + '</b> <br>'
 
 							# row[2] is the invoice number
-							InvoiceNumber = '*' + str.lstrip(row[2]) + '*' # removes spaces to the left of the row[2]
+							# below removes spaces to the left of the row[2]
+							InvoiceNumber = '*' + str.lstrip(row[2]) + '*'
 							for doc in os.listdir(self.attachmentsDirectory):
 								if fnmatch.fnmatch(doc, InvoiceNumber):
 									attachmentPath = os.path.join(self.attachmentsDirectory, doc)
@@ -238,13 +251,17 @@ class BottomToolbar:
 		class Sending(Body):
 			msg['From'] = senderMailAddress
 			msg['To'] = receiverMailAddress
-			def __init__(self, senderMailPassword, fileToAutoRemove, *args, **kwargs):
+			def __init__(self, senderMailPassword, fileToAutoRemove,
+						 *args, **kwargs):
 				super().__init__(*args, **kwargs)
 				self.senderMailPassword = senderMailPassword
-				self.fileToAutoRemove = fileToAutoRemove # abstract file which is created and removed automatically (see Removing class)
+				# below is abstract file which is created and
+				# removed automatically (see Removing class)
+				self.fileToAutoRemove = fileToAutoRemove
 
 			def sendMail(self):
-				server = smtplib.SMTP_SSL(hostofSenderMail, 465) # 465 is a port for sender's mail
+				# 465 is a port for sender's mail
+				server = smtplib.SMTP_SSL(hostofSenderMail, 465)
 				server.ehlo()
 				server.login(msg['From'], self.senderMailPassword)
 				server.sendmail(msg['From'], msg['To'], msg.as_string())
@@ -258,15 +275,18 @@ class BottomToolbar:
 		sendingObject.sendMail()
 
 		class Removing:
-			def __init__(self, archiveForAttachmentsPath, contentToSend, fileToAutoRemove):
+			def __init__(self, archiveForAttachmentsPath, contentToSend,
+					     fileToAutoRemove):
 				self.archiveForAttachmentsPath = archiveForAttachmentsPath
 				self.contentToSend = contentToSend
 				self.fileToAutoRemove = fileToAutoRemove
 
 			def removeAbstractFile(self):
-				openedContentToSend = open(self.contentToSend, 'r', encoding='utf-8')
+				openedContentToSend = open(self.contentToSend, 'r',
+										   encoding='utf-8')
 				readContent = csv.reader(openedContentToSend, delimiter=';')
-				openedFileToAutoRemove = open(self.fileToAutoRemove, 'w', encoding='utf-8', newline='')
+				openedFileToAutoRemove = open(self.fileToAutoRemove, 'w',
+											  encoding='utf-8', newline='')
 				writer = csv.writer(openedFileToAutoRemove,  delimiter=';')
 				for row in readContent:
 					row[0]='sent'
@@ -287,15 +307,19 @@ class BottomToolbar:
 				super().__init__(*args, **kwargs)
 
 			def move(self):
-				with open(self.contentToSend, encoding='utf-8') as openedContentToSend:
-					readContent = csv.reader(openedContentToSend, delimiter =';')
+				with open(self.contentToSend,
+				encoding='utf-8') as openedContentToSend:
+					readContent = csv.reader(openedContentToSend,
+											 delimiter =';')
 					for row in readContent:
 						InvoiceNumber = '*' + str.lstrip(row[2]) + '*'
 						for doc in os.listdir(self.attachmentsDirectory):
 							if fnmatch.fnmatch(doc, InvoiceNumber):
 								if '.DS_Store' in self.archiveForAttachmentsPath:
-										os.remove(self.archiveForAttachmentsPath + '.DS_Store')
-								shutil.move(self.attachmentsDirectory + doc, self.archiveForAttachmentsPath)
+										os.remove(self.archiveForAttachmentsPath +
+												  '.DS_Store')
+								shutil.move(self.attachmentsDirectory + doc,
+											self.archiveForAttachmentsPath)
 
 		moving = Moving(attachmentsDirectory,
 						archiveForAttachmentsPath,
@@ -320,24 +344,41 @@ bottomToolbarObject = BottomToolbar()
 
 # File menu
 filemenu = Menu(menubar, tearoff=0 )
-filemenu.add_command(label = "New (clear screen)", accelerator = 'Cmd+N', compound = LEFT, image = newMenuIcon, underline = 0, command = fileMenuObject.new_file)
-filemenu.add_command(label = "Save", accelerator = 'Cmd+S',compound = LEFT, image = saveMenuIcon,underline = 0, command=bottomToolbarObject.saveBig)
-filemenu.add_command(label = "Save as",accelerator = 'Shift+Ctrl+S', compound = LEFT, image = saveAsMenuIcon,underline = 0, command=fileMenuObject.save_as_function)
-filemenu.add_command(label = "Export to CSV",accelerator = 'Ctrl+E', compound = LEFT, image = exportMenuIcon,underline = 0, command=bottomToolbarObject.exportBig)
+filemenu.add_command(label = "New (clear screen)", accelerator = 'Cmd+N',
+			         compound = LEFT, image = newMenuIcon, underline = 0,
+					 command = fileMenuObject.new_file)
+filemenu.add_command(label = "Save", accelerator = 'Cmd+S',
+					 compound = LEFT, image = saveMenuIcon,underline = 0,
+					 command=bottomToolbarObject.saveBig)
+filemenu.add_command(label = "Save as",accelerator = 'Shift+Ctrl+S',
+					 compound = LEFT, image = saveAsMenuIcon,underline = 0,
+					 command=fileMenuObject.save_as_function)
+filemenu.add_command(label = "Export to CSV",accelerator = 'Ctrl+E',
+					 compound = LEFT, image = exportMenuIcon,underline = 0,
+					 command=bottomToolbarObject.exportBig)
 filemenu.add_separator()
-filemenu.add_command(label = "Exit", accelerator = 'Alt+F4', compound = LEFT, image = exitMenuIcon,underline = 0, command = fileMenuObject.exit_editor)
+filemenu.add_command(label = "Exit", accelerator = 'Alt+F4',
+					 compound = LEFT, image = exitMenuIcon,underline = 0,
+					 command = fileMenuObject.exit_editor)
 menubar.add_cascade(label = "File", menu = filemenu)
 
 # Edit menu
 editmenu = Menu(menubar, tearoff = 0)
-editmenu.add_command(label="Undo",compound=LEFT, image=undoMenuIcon, accelerator='Cmd+Z', command = editMenuObject.undo)
-editmenu.add_command(label="Redo",compound=LEFT, image=redoMenuIcon, accelerator='Cmd+Y', command = editMenuObject.redo)
+editmenu.add_command(label="Undo",compound=LEFT, image=undoMenuIcon,
+				     accelerator='Cmd+Z', command = editMenuObject.undo)
+editmenu.add_command(label="Redo",compound=LEFT, image=redoMenuIcon,
+					 accelerator='Cmd+Y', command = editMenuObject.redo)
 editmenu.add_separator()
-editmenu.add_command(label="Cut", compound=LEFT, image=cutMenuIcon, accelerator='Cmd+X', command = editMenuObject.cut)
-editmenu.add_command(label="Copy", compound=LEFT, image=copyMenuIcon,  accelerator='Cmd+C', command = editMenuObject.copy)
-editmenu.add_command(label="Paste",compound=LEFT, image=pasteMenuIcon, accelerator='Cmd+V', command = editMenuObject.paste)
+editmenu.add_command(label="Cut", compound=LEFT, image=cutMenuIcon,
+					 accelerator='Cmd+X', command = editMenuObject.cut)
+editmenu.add_command(label="Copy", compound=LEFT, image=copyMenuIcon,
+					 accelerator='Cmd+C', command = editMenuObject.copy)
+editmenu.add_command(label="Paste",compound=LEFT, image=pasteMenuIcon,
+	    			 accelerator='Cmd+V', command = editMenuObject.paste)
 editmenu.add_separator()
-editmenu.add_command(label="Find", compound=LEFT, image=findMenuIcon, underline=0, accelerator='Cmd+F', command=editMenuObject.on_find)
+editmenu.add_command(label="Find", compound=LEFT, image=findMenuIcon,
+					 underline=0, accelerator='Cmd+F',
+					 command=editMenuObject.on_find)
 editmenu.add_separator()
 menubar.add_cascade(label = "Edit ", menu=editmenu)
 
@@ -352,12 +393,16 @@ root.bind('<Command-s>', bottomToolbarObject.saveBig)
 
 # toolbar
 shortcutbar = Frame(root, height=25, bg='gainsboro')
-icons = ['bottomToolbarObject.openBig', 'bottomToolbarObject.saveBig', 'bottomToolbarObject.exportBig', 'bottomToolbarObject.sendBig']
+icons = ['bottomToolbarObject.openBig',
+ 		 'bottomToolbarObject.saveBig',
+		 'bottomToolbarObject.exportBig',
+		 'bottomToolbarObject.sendBig']
 for i, icon in enumerate(icons):
 	pathForPictures = 'pictures/'+icon+'.gif'
 	tbicon = PhotoImage(file=pathForPictures.replace('bottomToolbarObject.', ''))
 	cmd = eval(icon)
-	toolbar = Button(shortcutbar, image=tbicon, command=cmd, activeforeground='dodgerblue3')
+	toolbar = Button(shortcutbar, image=tbicon, command=cmd,
+			  activeforeground='dodgerblue3')
 	toolbar.image = tbicon
 	toolbar.pack(padx=70, pady=5, side=LEFT)
 shortcutbar.pack(expand=NO, fill=X)
