7aaff1c16b7e52d804319cedbe005facea28cb4f
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-01-13 19:18:29 +0100

    Adding first unittest test for EditMenu object

--- GIT_emailFV_test.py
+++ GIT_emailFV_test.py
@@ -4,3 +4,7 @@ from GIT_emailFV import EditMenu
 class EditMenuTest(unittest.TestCase):
     def test_initialization(self):
         editMenuObject = EditMenu()
+        self.assertTrue(editMenuObject) # checks if object of class EditMenu exists
+
+if __name__ == '__main__':
+    unittest.main()
